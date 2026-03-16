a7f13724bb393b4db96072bdf32f1fdbfc42b6d4
ommel2006 <emi.puchaicela@gmail.com>
2026-03-16 12:46:55 -0600

    Added progress tracking functions, edited styled and added buttons

--- res/js/app.js
+++ res/js/app.js
@@ -1,6 +1,7 @@
 var rejections = JSON.parse(localStorage.getItem('rejections')) || [];
 const outcomes = ['Rejected', 'Pending', 'Accepted'];
 var objective = JSON.parse(localStorage.getItem('objective')) || 1000;
+var progress = JSON.parse(localStorage.getItem('progress')) || rejections.filter(r => r.outcome === 'Rejected').length;
 
 function saveRejections() {
     localStorage.setItem('rejections', JSON.stringify(rejections));
@@ -9,6 +10,10 @@ function saveRejections() {
 function addRejection(description, category, outcome, date, notes = null){
     const newRejection = new Rejection(description, category, outcome, date, notes);
     rejections.push(newRejection);
+    if (outcome === 'Rejected') {
+        progress++;
+        saveProgress();
+    }
     saveRejections();
 }
 
@@ -18,6 +23,10 @@ function editRejection(id, description = null, category = null, outcome = null, 
         if (description !== null) rejection.editDescription(description);
         if (category !== null) rejection.editCategory(category);
         if (outcome !== null) rejection.editOutcome(outcome);
+        if (outcome === 'Rejected') {
+            progress++;
+            saveProgress();
+        }
         if (date !== null) rejection.editDate(date);
         if (notes !== null) rejection.editNotes(notes);
         saveRejections();
@@ -25,8 +34,15 @@ function editRejection(id, description = null, category = null, outcome = null, 
 }
 
 function deleteRejection(id) {
-    rejections = rejections.filter(r => r.id !== id);
-    saveRejections();
+    const rejection = rejections.find(r => r.id === id);
+    if (rejection) {
+        if (rejection.outcome === 'Rejected') {
+            progress--;
+            saveProgress();
+        }
+        rejections = rejections.filter(r => r.id !== id);
+        saveRejections();
+    }
 }
 
 function saveObjective() {
@@ -36,4 +52,8 @@ function saveObjective() {
 function editObjective(newObjective) {
     objective = newObjective;
     saveObjective();
+}
+
+function saveProgress() {
+    localStorage.setItem('progress', JSON.stringify(progress));
 }
